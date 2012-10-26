31c44b9dfa4326e9941613c84de0fac491b8f978
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2012-10-26 12:01:39 -0400

    cleaning

--- bloodline.hs
+++ bloodline.hs
@@ -17,21 +17,75 @@ nameSelect name = do
 
 	let nameLangLit = (name ++ " (programming language)", "en")
 
-	triple programmingLanguage (rdf .:. "type") (dbpediaOwl .:. "ProgrammingLanguage")
+	-- triple programmingLanguage (rdf .:. "type") (dbpediaOwl .:. "ProgrammingLanguage")
 	triple programmingLanguage (rdfs .:. "label") nameLangLit
 
 	return SelectQuery { queryVars = [programmingLanguage] }
 
+influencedBySelect :: String -> Query SelectQuery
+influencedBySelect name = do
+	rdf <- prefix "rdf" (iriRef "http://www.w3.org/1999/02/22-rdf-syntax-ns#")
+	rdfs <- prefix "rdfs" (iriRef "http://www.w3.org/2000/01/rdf-schema#")
+	dbpediaOwl <- prefix "dbpedia-owl" (iriRef "http://dbpedia.org/ontology/")
+	dbprop <- prefix "dbprop" (iriRef "http://dbpedia.org/property/")
+
+	programmingLanguage <- var
+	influencedBy <- var
+
+	let nameLangLit = (name ++ " (programming language)", "en")
+
+	triple programmingLanguage (rdfs .:. "label") nameLangLit
+	triple programmingLanguage (dbpediaOwl .:. "influencedBy") influencedBy
+
+	return SelectQuery { queryVars = [influencedBy] }
+
+influencedSelect :: String -> Query SelectQuery
+influencedSelect name = do
+	rdf <- prefix "rdf" (iriRef "http://www.w3.org/1999/02/22-rdf-syntax-ns#")
+	rdfs <- prefix "rdfs" (iriRef "http://www.w3.org/2000/01/rdf-schema#")
+	dbpediaOwl <- prefix "dbpedia-owl" (iriRef "http://dbpedia.org/ontology/")
+	dbprop <- prefix "dbprop" (iriRef "http://dbpedia.org/property/")
+
+	programmingLanguage <- var
+	influenced <- var
+
+	let nameLangLit = (name ++ " (programming language)", "en")
+
+	triple programmingLanguage (rdfs .:. "label") nameLangLit
+	triple programmingLanguage (dbpediaOwl .:. "influenced") influenced
+
+	return SelectQuery { queryVars = [influenced] }
+
 dbpediaEndpoint :: String
 dbpediaEndpoint = "http://dbpedia.org/sparql"
+
+-- like Data.Maybe (catMaybes)
+catBounds :: [BindingValue] -> [Node]
+catBounds ls = [x | Bound x <- ls]
+
+unbind :: Maybe [[BindingValue]] -> [[Node]]
+unbind results = case results of
+	Nothing -> []
+	Just results' -> map catBounds results'
 
 langResource :: String -> IO String
 langResource name = do
 	results <- selectQuery dbpediaEndpoint (nameSelect name)
+	return $ show $ unbind results
 
-	return $ show results
+langParents :: String -> IO String
+langParents name = do
+	results <- selectQuery dbpediaEndpoint (influencedBySelect name)
+	return $ show $ unbind results
+
+langChildren :: String -> IO String
+langChildren name = do
+	results <- selectQuery dbpediaEndpoint (influencedSelect name)
+	return $ show $ unbind results
 
 main :: IO ()
 main = do
-	results <- langResource "D"
+	-- results <- langResource "D"
+	-- results <- langParents "D"
+	results <- langChildren "D"
 	putStrLn $ "Results: " ++ results
